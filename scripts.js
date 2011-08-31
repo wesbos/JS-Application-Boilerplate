6c0bc8ab9ed2e0d20f7bb2617bed10a44fed14ff
Wes Bos <wesbos@gmail.com>
2011-08-31 11:19:28 -0400

    formattin'

--- scripts.js
+++ scripts.js
@@ -1,11 +1,10 @@
-
 /*************************************************************************/
 /* Application
 /*************************************************************************/
 (function(window, undefined){
 
-	// application object
-    var	App = {};
+  // application object
+  var App = {};
     
     // stores array of queued ajax requests
     App.queue = [];
@@ -18,47 +17,47 @@
     
     // pubsub
     App.publish = function(topic, args){
-	    App.subscriptions[topic] && $.each(App.subscriptions[topic], function(){
-	        this.apply(App, args || []);
-	    });
-	};
-	
-	App.subscribe = function(topic, callback){
-	    if(!App.subscriptions[topic]){
-	        App.subscriptions[topic] = [];
-	    }
-	    App.subscriptions[topic].push(callback);
-	    return [topic, callback];
-	};
-	
-	App.unsubscribe = function(handle){
-	    var t = handle[0];
-	    App.subscriptions[t] && $.each(App.subscriptions[t], function(idx){
-	        if(this == handle[1]){
-	            App.subscriptions[t].splice(idx, 1);
-	        }
-	    });
-	};
-		
-	/*************************************************************************/
-	/* Subscriptions
-	/*************************************************************************/
-	App.subscribe("init", function(){
-		
-	});
-	
-	/*************************************************************************/
-	/* Events
-	/*************************************************************************/
-	
-	// DOM Ready
-	jQuery(function($){
-	    App.publish("init");
-	});
-	
-	// Window unload
-	jQuery(window).unload(function(){
-	    App.publish("destroy");
-	});
+      App.subscriptions[topic] && $.each(App.subscriptions[topic], function(){
+          this.apply(App, args || []);
+      });
+    };
+  
+    App.subscribe = function(topic, callback){
+        if(!App.subscriptions[topic]){
+            App.subscriptions[topic] = [];
+        }
+        App.subscriptions[topic].push(callback);
+        return [topic, callback];
+    };
+    
+    App.unsubscribe = function(handle){
+        var t = handle[0];
+        App.subscriptions[t] && $.each(App.subscriptions[topic], function(idx){
+            if(this == handle[1]){
+                App.subscriptions[topic].splice(idx, 1);
+            }
+        });
+    };
+    
+  /*************************************************************************/
+  /* Subscriptions
+  /*************************************************************************/
+  App.subscribe("init", function(){
+    
+  });
+  
+  /*************************************************************************/
+  /* Events
+  /*************************************************************************/
+  
+  // DOM Ready
+  jQuery(function($){
+      App.publish("init");
+  });
+  
+  // Window unload
+  jQuery(window).unload(function(){
+      App.publish("destroy");
+  });
 
 })(window);
